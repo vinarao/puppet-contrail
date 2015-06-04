05a6eaff1193c81b71daf4a85ff34475057d5910
Yanis Guenane <yanis@guenane.org>
2015-06-05 00:53:09 +0200

    Creation of the contrail-webui role

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,5 +5,6 @@ class contrail::params {
   $control_package_name = ['contrail-openstack-control']
   $config_package_name = ['contrail-openstack-config']
   $analytics_package_name = ['contrail-openstack-analytics']
+  $webui_package_name = ['contrail-openstack-webui']
 
 }
