720c02d8fe4b457770067b278f22568bef00eda1
Yanis Guenane <yanis@guenane.org>
2015-06-09 15:46:09 +0200

    Creation of the contrail-vrouter role

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,5 +7,6 @@ class contrail::params {
   $analytics_package_name = ['contrail-openstack-analytics']
   $webui_package_name = ['contrail-openstack-webui']
   $database_package_name = ['contrail-openstack-database']
+  $vrouter_package_name = ['contrail-openstack-vrouter']
 
 }
