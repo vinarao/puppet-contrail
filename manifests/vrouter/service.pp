7687596016b2efc1108757365f67552172b34913
Michael Henkel <mhenkel@juniper.net>
2017-02-08 20:12:25 +0100

    fixup

--- manifests/vrouter/service.pp
+++ manifests/vrouter/service.pp
@@ -16,22 +16,6 @@ class contrail::vrouter::service(
     ensure => running,
     enable => true,
   }
-  $address = inline_template("<%= scope.lookupvar('::ipaddress_${physical_interface}') -%>")
-  if $address == $vhost_ip {
-    exec { 'ip address del':
-      path => '/sbin',
-      command => "ip addr del ${vhost_ip}/${cidr} dev ${physical_interface}",
-      refreshonly => true,
-    }
-  }
-  $cur_gateway = get_gateway()
-  if $cur_gateway != $gateway {
-    exec { 'add default gw':
-      path => '/sbin',
-      command => "ip route add default via ${gateway}",
-      refreshonly => true,
-    }
-  }
   if $step == 5 and !$is_tsn {
     exec { 'restart nova compute':
       path => '/bin',
