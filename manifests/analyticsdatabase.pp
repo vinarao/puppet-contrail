153c433cccc7154b10edaa1a0275ac09b5cfb7ed
Michael Henkel <mhenkel@juniper.net>
2017-02-16 16:42:14 -0800

    fixup

--- manifests/analyticsdatabase.pp
+++ manifests/analyticsdatabase.pp
@@ -13,7 +13,7 @@ class contrail::analyticsdatabase (
 ) inherits contrail::params {
 
   anchor {'contrail::analyticsdatabase::start': } ->
-  class {'::contrail::database::install': } ->
+  class {'::contrail::analyticsdatabase::install': } ->
   class {'::contrail::analyticsdatabase::config': 
     database_nodemgr_config => $analyticsdatabase_params['database_nodemgr_config'],
     cassandra_servers       => $analyticsdatabase_params['cassandra_servers'],
