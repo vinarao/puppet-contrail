77945ef6a5db00fdee40a42ded5b1e7c9e3ac690
Michael Henkel <michael.henkel@gmail.com>
2017-05-29 15:22:17 -0400

    dpdk install checks

--- manifests/vrouter/install.pp
+++ manifests/vrouter/install.pp
@@ -37,26 +37,26 @@ class contrail::vrouter::install (
     exec { 'set selinux to permissive' :
       command => '/sbin/setenforce permissive',
     }
-    file {'model.py.patch' :
+    file {'/model.py.patch' :
       ensure  => file,
       source => '/usr/share/openstack-puppet/modules/contrail/files/vrouter/model.py.patch',
     } ->
-    file {'os_vif_util.py.patch' :
+    file {'/os_vif_util.py.patch' :
       ensure  => file,
       source => '/usr/share/openstack-puppet/modules/contrail/files/vrouter/os_vif_util.py.patch',
     } ->
-    file {'vif.py.patch' :
+    file {'/vif.py.patch' :
       ensure  => file,
       source => '/usr/share/openstack-puppet/modules/contrail/files/vrouter/vif.py.patch',
     } ->
     exec { 'patch -p0 < model.py.patch':
-      command => '/bin/patch -p0 < model.py.patch',
+      command => '/bin/patch -p0 < /model.py.patch',
     } ->
     exec { 'patch -p0 < os_vif_util.py.patch':
-      command => '/bin/patch -p0 < os_vif_util.py.patch',
+      command => '/bin/patch -p0 < /os_vif_util.py.patch',
     } ->
     exec { 'patch -p0 < vif.py.patch':
-      command => '/bin/patch -p0 < vif.py.patch',
+      command => '/bin/patch -p0 < /vif.py.patch',
     }
   }
   exec { '/sbin/weak-modules --add-kernel' :
