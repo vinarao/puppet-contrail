ac0810fe4831bdbc90485b6c2dacc5cf14f358ff
Michael Henkel <mhenkel@juniper.net>
2017-08-31 17:59:27 +0100

    Set enforce only if permissive
    Closes Bug: 1706741
    
    Change-Id: I150d1519f4bf9680d6f4dbd2bd6b0e996913487a

--- manifests/vrouter/install.pp
+++ manifests/vrouter/install.pp
@@ -48,6 +48,7 @@ class contrail::vrouter::install (
     }
     exec { 'set selinux to permissive' :
       command => '/sbin/setenforce permissive',
+      onlyif  => 'sestatus | grep -i "Current mode" | grep -q enforcing',
     }
     file_line { 'make permissive mode persistant':
       ensure => present,
