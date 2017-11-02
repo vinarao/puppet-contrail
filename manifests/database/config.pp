6a3415a34a3a8ecd8ae917a8f0a850b5da0140fa
alexey-mr <alexey.morlang@gmail.com>
2017-11-02 10:28:54 +0300

    use package_name cassandra and hints directory
    w/o package_name puppet-cassandra uses cassandra22 package on RedHat.
    w/o providing hints directory cassandra-3 uses /hints dir on RedHat.
    
    Change-Id: I6890f3266fa3f952dd87967f104d9d33f1ea8eb6
    Closes-Bug: #1729055

--- manifests/database/config.pp
+++ manifests/database/config.pp
@@ -45,9 +45,11 @@ class contrail::database::config (
     mode   => '0755',
   } ->
   class {'::cassandra':
+    package_name      => 'cassandra',
+    hints_directory   => '/var/lib/cassandra/hints',
 #    service_ensure => stopped,
 #    service_enable => false,
-    settings => {
+    settings          => {
       'cluster_name'          => 'ConfigDatabase',
       'listen_address'        => $cassandra_ip,
       'storage_port'          => $storage_port,
