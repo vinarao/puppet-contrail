93eae4225074f5c5e0de7a840b4e2cf2aa790177
tikitavi <rtikitavi@gmail.com>
2017-11-03 16:10:00 +0300

    do not set VXLAN Identifier Mode by default
    Change-Id: I3b0c33d538e47f32eab771e80178b1298c4daa7a
    Closes-Bug: #1721811

--- manifests/control/provision_encap.pp
+++ manifests/control/provision_encap.pp
@@ -44,12 +44,17 @@ class contrail::control::provision_encap (
   $api_address                = '127.0.0.1',
   $api_port                   = 8082,
   $encap_priority             = 'MPLSoUDP,MPLSoGRE,VXLAN',
-  $vxlan_vn_id_mode           = 'automatic',
+  $vxlan_vn_id_mode           = undef,
   $keystone_admin_user        = 'admin',
   $keystone_admin_password    = 'password',
   $keystone_admin_tenant_name = 'admin',
   $oper                       = 'add',
 ) {
+  if $vxlan_vn_id_mode {
+    $vxlan_vn_id_mode_opt = "--vxlan_vn_id_mode ${vxlan_vn_id_mode}"
+  } else {
+    $vxlan_vn_id_mode_opt = ''
+  }
 
   exec { "provision_encap.py ${api_address}" :
     path => '/usr/bin',
@@ -57,7 +62,7 @@ class contrail::control::provision_encap (
                  --api_server_ip ${api_address} \
                  --api_server_port ${api_port} \
                  --encap_priority ${encap_priority} \
-                 --vxlan_vn_id_mode ${vxlan_vn_id_mode} \
+                 ${vxlan_vn_id_mode_opt} \
                  --admin_user ${keystone_admin_user} \
                  --admin_password ${keystone_admin_password} \
                  --oper ${oper}",
