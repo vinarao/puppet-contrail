8215081143cb5d78e6b08d3c74e06fb30cbef9da
VinayRao <vinayrao@juniper.net>
2018-03-02 20:13:15 -0500

    Adding yum lockversion to avoid Java version updates

--- manifests/webui/install.pp
+++ manifests/webui/install.pp
@@ -9,9 +9,14 @@
 #
 class contrail::webui::install (
 ) {
-
+  package { 'yum-plugin-versionlock' :
+    ensure => 'latest',
+  } ->
+  exec { 'lock java version':
+    command => 'yum versionlock java-1.8.0-openjdk-*',
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+  } ->
   package { 'contrail-openstack-webui' :
     ensure => latest,
   }
-
 }
